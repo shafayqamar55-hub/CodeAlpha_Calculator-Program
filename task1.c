1318255e62beea1eec314b2ec08f2db2faba8fce
Shafay Qamar <shafayqamar55@gmail.com>
2026-08-25 00:34:21 -0700

    Fix formatting and syntax in basic calculator program

--- task1.c
+++ task1.c
@@ -18,7 +18,7 @@ int main()
 
    int c;
 
-   printf("1 for Addition \n 2 for Subtracton \n 3 for Multiplication \n 4 for Division \n ");
+   printf(" 1 for Addition \n 2 for Subtracton \n 3 for Multiplication \n 4 for Division \n ");
 
     printf("Enter the Choice:");
    scanf("%d", &c);
